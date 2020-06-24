ec11ef30b13f47c5354af8155f61cac790855ff0
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-06-24 17:58:33 +0530

    some

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,13 +4,41 @@ contract Token {
     // constructor
     // set the total no. of tokens
     // read the total no. of tokens
+
+    // NAME
+    string public name = "Nevermind_Token";
+
+    string public symbol = "NEV";
+
+    string public standard = "NEV token V1";
+
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceof;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     constructor(uint256 _initialSupply) public {
         balanceof[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceof[msg.sender] >= _value, "Insufficient balance"); /// check
+
+        balanceof[msg.sender] -= _value; // deduct from current
+
+        balanceof[_to] += _value; // add to _to
+
+        emit Transfer(msg.sender, _to, _value); // emit event.
+        return true;
+    }
+
+
+        
+
 
 }
--- test/Token.js
+++ test/Token.js
@@ -12,4 +12,32 @@ contract('Token', (accounts) => {
 
       assert(adminBalance.toNumber() === 1000000);
    });
+   it('must have same name', async () => {
+      const token = await Token.deployed();
+      let name = await token.name();
+      assert(name === 'Nevermind_Token');
+   });
+
+   it('must have same symbol', async () => {
+      const token = await Token.deployed();
+      let symbol = await token.symbol();
+      assert(symbol === 'NEV');
+   });
+
+   it('must have standard', async () => {
+      const token = await Token.deployed();
+      let standard = await token.standard();
+      assert.equal(standard, 'NEV token V1', 'has correct standard');
+   });
+
+   it('must transfer', async () => {
+      const token = await Token.deployed();
+      await token.transfer(accounts[1], 1, {
+         from: accounts[0],
+      });
+      let balance = await token.balanceof(accounts[1]);
+      let remain = await token.balanceof(accounts[0]);
+      assert.equal(balance.toNumber(), 1);
+      console.log(remain.toNumber());
+   });
 });
