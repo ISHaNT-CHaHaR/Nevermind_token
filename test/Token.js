ee47101f7532a4601d499617e8c09fc7fba7f8bd
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-06-26 13:34:22 +0530

    Completed ERC 20 "

--- test/Token.js
+++ test/Token.js
@@ -5,6 +5,11 @@ contract('Token', (accounts) => {
       const token = await Token.deployed();
       let adminBalance = await token.balanceof(accounts[0]);
 
+      console.log(await token.name());
+      console.log(await token.symbol());
+      let total = await token.totalSupply();
+      console.log(await token.standard());
+      console.log(total.toNumber());
       assert(adminBalance.toNumber() === 1000000);
    });
 
